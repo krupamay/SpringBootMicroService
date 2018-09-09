82a7105d01e5d39a23056fe5770f91ac00babd7f
krupamayghosal@gmail.com <krupamayghosal@gmail.com>
2018-09-09 17:41:29 +0530

    added Authorised perosn service

---
+++ authorised-person-services/src/main/java/com/metademy/microservices/authorisedpersonservices/AuthorisedPersonServicesApplication.java
@@ -0,0 +1,12 @@
+package com.metademy.microservices.authorisedpersonservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorisedPersonServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorisedPersonServicesApplication.class, args);
+	}
+}
